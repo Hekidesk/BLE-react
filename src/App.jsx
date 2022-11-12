a98e1acabe06676a5fe0b55dd71d40f8ec7828b9
moxeed <mortezabahjat@gmail.com>
2022-11-12 15:46:20 +0330

    Ble Is Ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
 import "./App.css";
 
+const ServiceUUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+const CharistristicUUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+
 function App() {
-  const [state, onChange] = useState("");
+  const [state, setState] = useState("");
+  const [ppg, setPpg] = useState();
+  const [ecg, setEcg] = useState();
+  const [force, setForce] = useState();
 
   async function onButtonClick() {
     navigator.bluetooth
       .requestDevice({
+        optionalServices: [ServiceUUID],
         filters: [{ namePrefix: "ECG-PPG-Server" }],
       })
       .then((device) => {
         device.gatt.connect().then((gatt) => {
-          console.log(gatt);
-          onChange(gatt.device.name);
-          gatt.getPrimaryService("/ uuid ").then(console.log);
+          gatt.getPrimaryService(ServiceUUID).then((service) => {
+            service.getCharacteristic(CharistristicUUID).then((charastirctic) => {
+              charastirctic.oncharacteristicvaluechanged = (data) => {
+                setPpg(data.srcElement.value.getUint16(0, true))
+                setEcg(data.srcElement.value.getUint16(2, true))
+                setForce(data.srcElement.value.getUint16(4, true))
+              }
+              charastirctic.startNotifications()
+            })
+          });
         });
-        console.log(device);
       });
   }
   return (
     <>
       {state && <p>{state} connected!</p>}
+      {ppg && <p>{ppg} ppg!</p>}
+      {ecg && <p>{ecg} ecg!</p>}
+      {force && <p>{force} force!</p>}
       <button onClick={onButtonClick}>connect</button>
     </>
   );
