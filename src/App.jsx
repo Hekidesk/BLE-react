cd3a013f190b8be227aba3725308c1ba5c33cb5f
Parna Asadi <parna80as@gmail.com>
2022-11-12 15:04:43 +0330

    push origin navigator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [state, onChange] = useState("");
+
+  async function onButtonClick() {
+    navigator.bluetooth
+      .requestDevice({
+        filters: [{ namePrefix: "ECG-PPG-Server" }],
+      })
+      .then((device) => {
+        device.gatt.connect().then((gatt) => {
+          console.log(gatt);
+          onChange(gatt.device.name);
+          gatt.getPrimaryService("/ uuid ").then(console.log);
+        });
+        console.log(device);
+      });
+  }
+  return (
+    <>
+      {state && <p>{state} connected!</p>}
+      <button onClick={onButtonClick}>connect</button>
+    </>
+  );
 }
 
 export default App;
