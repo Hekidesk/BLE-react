b0689b649ad80cc784940acbb5088aa08df18210
moxeed <mortezabahjat@gmail.com>
2022-11-27 19:53:43 +0330

    HR_1 Converted And Executed

--- singal-processing/Processor.py
+++ singal-processing/Processor.py
@@ -4,30 +4,26 @@ import numpy as np
 from js import createObject
 from pyodide.ffi import create_proxy
 
-
 def HR_1(IR_data, time_axis):
-    print(type(IR_data))
-    print(type(time_axis))
 
+    IR_data_ = IR_data - IR_data.min()
+    vlocs, _ = sg.find_peaks(-IR_data_, distance=10)
+    vvlas = np.take(IR_data_, vlocs)
 
-    # array = np.array(IR_data)
-    # IR_data_ = array - array.min()
-    # vlocs, _ = sg.find_peaks(-IR_data_, distance=10)
+    f = interpolate.interp1d(vlocs, vvlas, bounds_error=False, fill_value=True)
+    n = np.arange(0, len(IR_data_))
 
-    # print(vlocs)
-    # vvlas = np.take(IR_data_, vlocs)
+    baseline = f(n)
+    out_IR = IR_data_ - baseline
+    thr = max(out_IR)/3
 
-    # f = interpolate.interp1d(vlocs, vvlas, bounds_error=False, fill_value=True)
-    # n = np.arange(0, len(IR_data_))
+    out_peaks, _ = sg.find_peaks(out_IR, distance=100, threshold=thr)
+    HR_1 = len(out_peaks) / time_axis * 60
 
-    # baseline = f(n)
-    # out_IR = IR_data_ - baseline
-    # thr = max(out_IR)/3
+    return HR_1
 
-    # out_peaks, _ = sg.find_peaks(out_IR, distance=100, threshold=thr)
-    # HR_1 = len(out_peaks) / time_axis * 60
+def HR_1_Adapter(IR_data, time_axis):
+    array = np.asarray(IR_data.to_py())
+    return HR_1(array, time_axis)
 
-    # return HR_1
-
-
-createObject(create_proxy(HR_1), "HR_1")
+createObject(create_proxy(HR_1_Adapter), "HR_1")
